c6cf717bdae20f626eeb7985a5444d145ab4196c
Tania Allard <t.allard@sheffield.ac.uk>
2017-10-23 16:21:26 +0100

    Cofing gh

--- scripts/deploy_ghpages.sh
+++ scripts/deploy_ghpages.sh
@@ -21,7 +21,6 @@ ORIGIN_URL=`git config --get remote.origin.url`
 URL_REPO=${ORIGIN_URL/\/\/github.com/\/\/$GIT_TOKEN@github.com}
 SHA=`git rev-parse --verify HEAD`
 
-echo "Origin: $ORIGIN_URL"
 
 GH_USER_NAME='trallard'
 GH_USER_EMAIL='t.allard@sheffield.ac.uk'
---
+++ scripts/gh_conf.sh
@@ -0,0 +1,16 @@
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+GH_USER_NAME='trallard'
+GH_USER_EMAIL='t.allard@sheffield.ac.uk'
+
+
+# configureing git
+git config --global user.email "$GH_USER_EMAIL"
+git config --global user.name "$GH_USER_EMAIL"
+
+
+# GitHub confing: saving this for later
+ORIGIN_URL=`git config --get remote.origin.url`
+
+echo "$ORIGIN_URL"
