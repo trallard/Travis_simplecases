75e324860fc8d83ca88f5c20cd8ae8659d4c8581
Tania Allard <t.allard@sheffield.ac.uk>
2017-10-23 16:26:39 +0100

    repo url

--- scripts/gh_conf.sh
+++ scripts/gh_conf.sh
@@ -12,5 +12,6 @@ git config --global user.name "$GH_USER_EMAIL"
 
 # GitHub confing: saving this for later
 ORIGIN_URL=`git config --get remote.origin.url`
+REPO_URL=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo "$ORIGIN_URL"
+echo "$REPO_URL"
