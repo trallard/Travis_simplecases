15917fab90d8be4bd0f511943b0212941eeddb1a
Tania Allard <t.allard@sheffield.ac.uk>
2017-10-24 10:31:24 +0100

    Pull branches

--- scripts/build_master.sh
+++ scripts/build_master.sh
@@ -8,47 +8,47 @@ TARGET_BRANCH="gh-pages"
 echo "********** Starting build  ********** "
 echo "********** Target branch: $TARGET_BRANCH ********** "
 
-# function doCompile{
-#   ./compile.sh
-# }
 
 # This script only builds when commits are made to the master branch
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $SOURCE_BRANCH ]; then
-    echo "Building on $SOURCE_BRANCH"
-    # doCompile
+    echo "Building on a branch that is not $SOURCE_BRANCH"
     exit 0
 fi
 
 
-# Clone the existing gh-pages for this repo into out/
+# Function used to pull all the branches
+function create_branches{
+  # Keep track of where Travis put us.
+  # We are on a detached head, and we need to be able to go back to it.
+  local build_head=$(git rev-parse HEAD)
+  echo "********** Head is $build_head$ **********"
+
+  # Fetch all the remote branches. Travis clones with `--depth`, which
+  # implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+  # do that.
+  git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+  git fetch
+  # optionally, we can also fetch the tags
+  git fetch --tags
+
+  # create the tacking branches
+  for branch in $(git branch -r|grep -v HEAD) ; do
+      git checkout -qf ${branch#origin/}
+  done
+
+  # finally, go back to where we were at the beginning
+  git checkout ${build_head}
+}
+
+
+# Run the create branches function
+create_branches
+
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
-git clone $REPO out
-cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 echo " ********** Creating new gh-pages branch ********** "
-cd ..
 
-# Clean out existing contents
-echo "********** Removing old static content ********** "
-rm -rf out/**/* || exit 0
-
-# Run our compile script
-# doCompile
-
-# Merge master into gh-pages
-git merge master
-
-# Now let's go have some fun with the cloned repo
-cd out
-git config user.name "$GH_USER_NAME"
-git config user.email "$GH_USER_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "**********  No changes to the output on this push; exiting. ********** "
-    exit 0
-fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
